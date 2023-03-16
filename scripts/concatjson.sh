ae2eedf8a1f7236bab75218bef0571a827ddcac7
Félix Giffard <technonerdzfg@gmail.com>
2023-03-16 19:54:10 -0400

    add simple script to concat json

---
+++ scripts/concatjson.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Remove the output file if it already exists
+rm -f "$output_file"
+
+# Add the opening bracket to the output file
+echo "["
+
+# Loop through all .json files in the directory
+for file in *.json
+do
+  # Concatenate the contents of the current file to the output file
+  cat "$file"
+  
+  # Add a comma before each file, except for the first one
+  if [ "$file" != "*.json" ]
+  then
+    echo ","
+  fi
+done
+
+# Add the closing bracket to the output file
+echo "]"
